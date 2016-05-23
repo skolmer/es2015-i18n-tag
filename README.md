f07e378e74acb4bebca2175f666b3dd06064e288
steffenk <steffen@kolmer.net>
2016-05-23 16:08:41 +0200

    Optimized configuration

--- README.md
+++ README.md
@@ -16,10 +16,25 @@ const name = 'Steffen'
 const amount = 1250.33
 
 // config i18n literals
-i18nConfig('de-DE', 'EUR', {
-    "Hello ${0}, you have ${1} in your bank account.": "Hallo ${0}, Sie haben ${1} auf Ihrem Bankkonto."
+i18nConfig({
+    locale: 'de-DE',
+    currency: 'EUR',
+    translations: {
+        "Hello ${0}, you have ${1} in your bank account.": "Hallo ${0}, Sie haben ${1} auf Ihrem Bankkonto."
+    },
+    number: {
+        // https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
+
+    },
+    date: {
+        // https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
+    }
 })
         
 i18n`Hello ${ name }, you have ${ amount }:c in your bank account.`
 // Hallo Steffen, Sie haben € 1,250.33 auf Ihrem Bankkonto.
 ```
+
+## Credits
+
+Thanks to @jaysoo for his initial draft of an [i18n template literal](http://jaysoo.ca/2014/03/20/i18n-with-es6-template-strings/)
--- test/test-es2015-i18n-tag.js
+++ test/test-es2015-i18n-tag.js
@@ -8,23 +8,50 @@ describe('i18n-tag-schema', () => {
         const name = 'Steffen'
         const amount = 1250.33
 
-        const actual = i18n`Hello ${ name }, you have ${ amount }:c in your bank account.`
-        
+        const actual = i18n`Hello ${name}, you have ${amount}:c in your bank account.`
+
         const expected = "Hello Steffen, you have $1,250.33 in your bank account."
         assert.equal(actual, expected);
     })
-    
+
     it(`should translate to german`, () => {
         const name = 'Steffen'
         const amount = 1250.33
-        
-        i18nConfig('de-DE', 'EUR', {
-            "Hello ${0}, you have ${1} in your bank account.": "Hallo ${0}, Sie haben ${1} auf Ihrem Bankkonto."
+
+        i18nConfig({
+            locale: 'de-DE',
+            currency: 'EUR',
+            translations: {
+                "Hello ${0}, you have ${1} in your bank account.": "Hallo ${0}, Sie haben ${1} auf Ihrem Bankkonto."
+            }
         })
 
-        const actual = i18n`Hello ${ name }, you have ${ amount }:c in your bank account.`
-        
+        const actual = i18n`Hello ${name}, you have ${amount}:c in your bank account.`
+
         const expected = "Hallo Steffen, Sie haben € 1,250.33 auf Ihrem Bankkonto."
+        assert.equal(actual, expected);
+    })
+    
+    it(`should have 12H date`, () => {
+        const name = 'Steffen'
+        const date = new Date(2012, 11, 20, 19, 0, 0);
+
+        i18nConfig({
+            date: { hour12: false }
+        })
+
+        let actual = i18n`Hello ${name}, the date is ${date}:t.`
+
+        let expected = "Hello Steffen, the date is 2012-12-20 19:00:00."
+        assert.equal(actual, expected);
+        
+        i18nConfig({
+            date: { hour12: true }
+        })
+
+        actual = i18n`Hello ${name}, the date is ${date}:t.`
+
+        expected = "Hello Steffen, the date is 2012-12-20 7:00:00 PM."
         assert.equal(actual, expected);
     })
 })
