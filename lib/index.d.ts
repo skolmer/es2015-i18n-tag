00a62557398b8ca49cad99be418fb88eff310fbd
steffenk <steffen@kolmer.net>
2016-05-24 05:22:34 +0200

    Added typings

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+// index.d.ts
+
+/**
+ * Handles i18n tagged template literals configuration
+ * 
+ * @param config Configuration object.
+ */
+export default function i18nConfig(config?: {locale?: string, currency?: string, translations?: {}, number?: {}, date?: {}}) : void
