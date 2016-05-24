3f7cd9eccb833d269755453aa5ccec44700f9bd5
steffenk <steffen@kolmer.net>
2016-05-24 06:13:10 +0200

    Updated typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,46 @@
 // index.d.ts
 
+type NumberConfig = {
+    localeMatcher: string,
+    style: string,
+    currency: string,
+    currencyDisplay: string,
+    useGrouping: boolean,
+    minimumIntegerDigits: number,
+    minimumFractionDigits: number,
+    maximumFractionDigits: number,
+    minimumSignificantDigits: number,
+    maximumSignificantDigits: number
+}
+
+type DateConfig = {
+    localeMatcher: string,
+    timeZone: string,
+    hour12: boolean,
+    formatMatcher: string,
+    weekday: string,
+    era: string,
+    year: number,
+    month: number,
+    day: number,
+    hour: number,
+    minute: number,
+    second: number,
+    timeZoneName: string
+}
+
+type Config = {locale?: string, currency?: string, translations?: {}, number?: NumberConfig, date?: DateConfig}
+
 /**
  * Handles i18n tagged template literals configuration
  * 
  * @param config Configuration object.
  */
-export default function i18nConfig(config?: {locale?: string, currency?: string, translations?: {}, number?: {}, date?: {}}) : void
+export function i18nConfig(config: Config) : void
+
+/**
+ * Transforms i18n tagged template literals
+ * 
+ * @param config Configuration object.
+ */
+export default function i18n(literals, ...values) : void
