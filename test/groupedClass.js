80fc21ef6dc6d5a1c5b27b8b093506a909a66a7b
Steffen Kolmer <steffen@kolmer.net>
2016-07-04 19:19:21 +0200

    Added tests for class decorators

---
+++ test/groupedClass.js
@@ -0,0 +1,27 @@
+import i18n, { i18nGroup } from '../lib/index'
+
+const name = 'Steffen'
+const amount = 1250.33
+
+@i18nGroup('custom group')
+export default class GroupedClass {
+    getText() {
+        return this.i18n`Hello ${name}, you have ${amount}:c in your bank account.`
+    }
+
+    getCommonText() {
+        return i18n('common')`Hello ${name}, you have ${amount}:c in your bank account.`
+    }
+}
+
+class GroupedClass2 {
+    getText() {
+        return this.i18n`Hello ${name}, you have ${amount}:c in your bank account.`
+    }
+
+    getCommonText() {
+        return i18n('common 2') `Hello ${name}, you have ${amount}:c in your bank account.`
+    }
+}
+
+export const groupedClass2 = i18nGroup('custom group 2')(GroupedClass2)
--- test/test-es2015-i18n-tag.js
+++ test/test-es2015-i18n-tag.js
@@ -1,12 +1,16 @@
 import assert from 'assert'
 import Intl from 'intl'
 import i18n, { i18nConfig }  from '../lib'
+import groupedClass, { groupedClass2 } from './groupedClass'
+
+const decorated = new groupedClass()
+const decorated2 = new groupedClass2()
 
 Intl.__applyLocaleSensitivePrototypes()
 global.Intl = Intl
 
 describe('es2015-i18n-tag', () => {
-    it(`should not translate`, () => {
+    it('should not translate', () => {
         const name = 'Steffen'
         const amount = 1250.33
 
@@ -19,18 +23,18 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`Hello ${name}, you have ${amount}:c in your bank account.`
 
-        const expected = "Hello Steffen, you have $1,250.33 in your bank account."
+        const expected = 'Hello Steffen, you have $1,250.33 in your bank account.'
         assert.equal(actual, expected)
     })
 
-    it(`should translate to german`, () => {
+    it('should translate to german', () => {
         const name = 'Steffen'
         const amount = 1250.33
 
         i18nConfig({
             locales: 'de-DE',
             translations: {
-                "Hello ${0}, you have ${1} in your bank account.": "Hallo ${0}, Sie haben ${1} auf Ihrem Bankkonto."
+                'Hello ${0}, you have ${1} in your bank account.': 'Hallo ${0}, Sie haben ${1} auf Ihrem Bankkonto.'
             },
             number: {
                 currency: 'EUR'
@@ -39,11 +43,11 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`Hello ${name}, you have ${amount}:c in your bank account.`
 
-        const expected = "Hallo Steffen, Sie haben 1.250,33 € auf Ihrem Bankkonto."
+        const expected = 'Hallo Steffen, Sie haben 1.250,33 € auf Ihrem Bankkonto.'
         assert.equal(actual, expected)
     })
 
-    it(`should format percentage`, () => {
+    it('should format percentage', () => {
         const name = 'Steffen'
         const percentage = 0.1
 
@@ -53,18 +57,18 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`Hello ${name}, the percentage is ${percentage}:p.`
 
-        const expected = "Hello Steffen, the percentage is 10%."
+        const expected = 'Hello Steffen, the percentage is 10%.'
         assert.equal(actual, expected)
     })
 
-    it(`should support nested templates`, () => {
+    it('should support nested templates', () => {
         i18nConfig({
             locales: 'en-US'
         })
 
         const hello = [
-            { name: "Steffen", percentage: 0.2 },
-            { name: "Jack", percentage: 0.8 }
+            { name: 'Steffen', percentage: 0.2 },
+            { name: 'Jack', percentage: 0.8 }
         ]
 
         const actual = i18n`
@@ -78,7 +82,7 @@ describe('es2015-i18n-tag', () => {
         assert.equal(actual, expected)
     })
 
-    it(`should support standard date format string "d"`, () => {
+    it('should support standard date format string "d"', () => {
         const date = new Date('Thu, 20 Dec 2012 18:00:00 UTC')
 
         i18nConfig({
@@ -90,12 +94,12 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`The date is ${date}:t(d).`
 
-        const expected = "The date is 12/20/2012."
+        const expected = 'The date is 12/20/2012.'
         assert.equal(actual, expected)
 
     })
 
-    it(`should support standard date format string "D"`, () => {
+    it('should support standard date format string "D"', () => {
         const date = new Date('Thu, 20 Dec 2012 18:00:00 UTC')
 
         i18nConfig({
@@ -107,12 +111,12 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`The date is ${date}:t(D).`
 
-        const expected = "The date is Thursday, December 20, 2012."
+        const expected = 'The date is Thursday, December 20, 2012.'
         assert.equal(actual, expected)
 
     })
 
-    it(`should support standard date format string "f"`, () => {
+    it('should support standard date format string "f"', () => {
         const date = new Date('Thu, 20 Dec 2012 18:00:00 UTC')
 
         i18nConfig({
@@ -124,12 +128,12 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`The date is ${date}:t(f).`
 
-        const expected = "The date is Thursday, December 20, 2012 at 6:00 PM."
+        const expected = 'The date is Thursday, December 20, 2012 at 6:00 PM.'
         assert.equal(actual, expected)
 
     })
 
-    it(`should support standard date format string "F"`, () => {
+    it('should support standard date format string "F"', () => {
         const date = new Date('Thu, 20 Dec 2012 18:00:00 UTC')
 
         i18nConfig({
@@ -141,12 +145,12 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`The date is ${date}:t(F).`
 
-        const expected = "The date is Thursday, December 20, 2012 at 6:00:00 PM."
+        const expected = 'The date is Thursday, December 20, 2012 at 6:00:00 PM.'
         assert.equal(actual, expected)
 
     })
 
-    it(`should support standard date format string "g"`, () => {
+    it('should support standard date format string "g"', () => {
         const date = new Date('Thu, 20 Dec 2012 18:00:00 UTC')
 
         i18nConfig({
@@ -158,12 +162,12 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`The date is ${date}:t(g).`
 
-        const expected = "The date is 12/20/2012, 6:00 PM."
+        const expected = 'The date is 12/20/2012, 6:00 PM.'
         assert.equal(actual, expected)
 
     })
 
-    it(`should support standard date format string "G"`, () => {
+    it('should support standard date format string "G"', () => {
         const date = new Date('Thu, 20 Dec 2012 18:00:00 UTC')
 
         i18nConfig({
@@ -175,12 +179,12 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`The date is ${date}:t(G).`
 
-        const expected = "The date is 12/20/2012, 6:00:00 PM."
+        const expected = 'The date is 12/20/2012, 6:00:00 PM.'
         assert.equal(actual, expected)
 
     })
-    
-    it(`should support standard date format string "M", "m"`, () => {
+
+    it('should support standard date format string "M", "m"', () => {
         const date = new Date('Thu, 20 Dec 2012 18:00:00 UTC')
 
         i18nConfig({
@@ -192,11 +196,11 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`The date is ${date}:t(m).`
 
-        const expected = "The date is December 20."
+        const expected = 'The date is December 20.'
         assert.equal(actual, expected)
     })
 
-    it(`should support standard date format string "O", "o"`, () => {
+    it('should support standard date format string "O", "o"', () => {
         const date = new Date('Thu, 20 Dec 2012 18:00:00 UTC')
 
         i18nConfig({
@@ -208,11 +212,11 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`The date is ${date}:t(o).`
 
-        const expected = "The date is 2012-12-20T18:00:00.000Z."
+        const expected = 'The date is 2012-12-20T18:00:00.000Z.'
         assert.equal(actual, expected)
     })
 
-    it(`should support standard date format string "R", "r"`, () => {
+    it('should support standard date format string "R", "r"', () => {
         const date = new Date('Thu, 20 Dec 2012 18:00:00 UTC')
 
         i18nConfig({
@@ -224,11 +228,11 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`The date is ${date}:t(r).`
 
-        const expected = "The date is Thu, 20 Dec 2012 18:00:00 GMT."
+        const expected = 'The date is Thu, 20 Dec 2012 18:00:00 GMT.'
         assert.equal(actual, expected)
     })
-    
-    it(`should support standard date format string "t"`, () => {
+
+    it('should support standard date format string "t"', () => {
         const date = new Date('Thu, 20 Dec 2012 18:00:00 UTC')
 
         i18nConfig({
@@ -240,12 +244,12 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`The date is ${date}:t(t).`
 
-        const expected = "The date is 6:00 PM."
+        const expected = 'The date is 6:00 PM.'
         assert.equal(actual, expected)
 
     })
-    
-    it(`should support standard date format string "T"`, () => {
+
+    it('should support standard date format string "T"', () => {
         const date = new Date('Thu, 20 Dec 2012 18:00:00 UTC')
 
         i18nConfig({
@@ -257,12 +261,12 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`The date is ${date}:t(T).`
 
-        const expected = "The date is 6:00:00 PM."
+        const expected = 'The date is 6:00:00 PM.'
         assert.equal(actual, expected)
 
     })
-    
-    it(`should support standard date format string "Y", "y"`, () => {
+
+    it('should support standard date format string "Y", "y"', () => {
         const date = new Date('Thu, 20 Dec 2012 18:00:00 UTC')
 
         i18nConfig({
@@ -274,7 +278,7 @@ describe('es2015-i18n-tag', () => {
 
         const actual = i18n`The date is ${date}:t(y).`
 
-        const expected = "The date is December 2012."
+        const expected = 'The date is December 2012.'
         assert.equal(actual, expected)
     })
 
@@ -302,7 +306,7 @@ describe('es2015-i18n-tag', () => {
         assert.equal(actual, expected)
     })
 
-    it(`should support translation groups`, () => {
+    it('should support translation groups', () => {
         const name = 'Steffen'
         const amount = 1250.33
 
@@ -310,21 +314,21 @@ describe('es2015-i18n-tag', () => {
             locales: 'de-DE',
             translations: {
                 testgroup: {
-                    "Hello ${0}, you have ${1} in your bank account.": "Hallo ${0}, Sie haben ${1} auf Ihrem Bankkonto."
-                }    
+                    'Hello ${0}, you have ${1} in your bank account.': 'Hallo ${0}, Sie haben ${1} auf Ihrem Bankkonto.'
+                }
             },
             number: {
                 currency: 'EUR'
             }
         })
 
-        const actual = i18n('testgroup')`Hello ${name}, you have ${amount}:c in your bank account.`
+        const actual = i18n('testgroup') `Hello ${name}, you have ${amount}:c in your bank account.`
 
-        const expected = "Hallo Steffen, Sie haben 1.250,33 € auf Ihrem Bankkonto."
+        const expected = 'Hallo Steffen, Sie haben 1.250,33 € auf Ihrem Bankkonto.'
         assert.equal(actual, expected)
     })
 
-    it(`should support translation group config`, () => {
+    it('should support translation group config', () => {
         const name = 'Steffen'
         const amount = 1250.33
 
@@ -333,25 +337,106 @@ describe('es2015-i18n-tag', () => {
             group: 'dafaultgroup',
             translations: {
                 testgroup: {
-                    "Hello ${0}, you have ${1} in your bank account.": "Hallo ${0}, Sie haben ${1} auf Ihrem Bankkonto."
+                    'Hello ${0}, you have ${1} in your bank account.': 'Hallo ${0}, Sie haben ${1} auf Ihrem Bankkonto.'
                 },
                 dafaultgroup: {
-                    "Hello ${0}, you have ${1} in your bank account.": "Hallo ${0}"
-                }     
+                    'Hello ${0}, you have ${1} in your bank account.': 'Hallo ${0}'
+                }
             },
             number: {
                 currency: 'EUR'
             }
         })
 
-        const actual = i18n('testgroup')`Hello ${name}, you have ${amount}:c in your bank account.`
+        const actual = i18n('testgroup') `Hello ${name}, you have ${amount}:c in your bank account.`
 
-        const expected = "Hallo Steffen, Sie haben 1.250,33 € auf Ihrem Bankkonto."
+        const expected = 'Hallo Steffen, Sie haben 1.250,33 € auf Ihrem Bankkonto.'
         assert.equal(actual, expected)
 
         const actualDefault = i18n`Hello ${name}, you have ${amount}:c in your bank account.`
-        const expectedDefault = "Hallo Steffen"
+        const expectedDefault = 'Hallo Steffen'
         assert.equal(actualDefault, expectedDefault)
     })
+
+    it('should support ES2016 style group class decorator', () => {
+        i18nConfig({
+            locales: 'de-DE',
+            translations: {
+                'custom group': {
+                    'Hello ${0}, you have ${1} in your bank account.': 'Hallo ${0}, Sie haben ${1} auf Ihrem Bankkonto.'
+                },
+                'common': {
+                    'Hello ${0}, you have ${1} in your bank account.': 'Hey ${0}'
+                }
+            },
+            number: {
+                currency: 'EUR'
+            }
+        })
+        const actual = decorated.getText()
+        const expected = 'Hallo Steffen, Sie haben 1.250,33 € auf Ihrem Bankkonto.'
+        assert.equal(actual, expected)
+    })
+
+    it('should support inline override of ES2016 decorator', () => {
+        i18nConfig({
+            locales: 'de-DE',
+            translations: {
+                'custom group': {
+                    'Hello ${0}, you have ${1} in your bank account.': 'Hallo ${0}, Sie haben ${1} auf Ihrem Bankkonto.'
+                },
+                'common': {
+                    'Hello ${0}, you have ${1} in your bank account.': 'Hey ${0}'
+                }
+            },
+            number: {
+                currency: 'EUR'
+            }
+        })
+        const actualCommon = decorated.getCommonText()
+        const expectedCommon = 'Hey Steffen'
+        assert.equal(actualCommon, expectedCommon)
+    })
+
+    it('should support ES2015 style group class decorator', () => {
+        i18nConfig({
+            locales: 'de-DE',
+            translations: {
+                'custom group 2': {
+                    'Hello ${0}, you have ${1} in your bank account.': 'Hallo ${0}'
+                },
+                'common 2': {
+                    'Hello ${0}, you have ${1} in your bank account.': 'Hey Ho ${0}'
+                }
+            },
+            number: {
+                currency: 'EUR'
+            }
+        })
+        const actual2 = decorated2.getText()
+        const expected2 = 'Hallo Steffen'
+        assert.equal(actual2, expected2)
+    })
+
+    it('should support inline override of ES2015 decorator', () => {
+        i18nConfig({
+            locales: 'de-DE',
+            translations: {
+                'custom group 2': {
+                    'Hello ${0}, you have ${1} in your bank account.': 'Hallo ${0}'
+                },
+                'common 2': {
+                    'Hello ${0}, you have ${1} in your bank account.': 'Hey Ho ${0}'
+                }
+            },
+            number: {
+                currency: 'EUR'
+            }
+        })
+        const actualCommon2 = decorated2.getCommonText()
+        const expectedCommon2 = 'Hey Ho Steffen'
+        assert.equal(actualCommon2, expectedCommon2)
+    })
 })
+
 
